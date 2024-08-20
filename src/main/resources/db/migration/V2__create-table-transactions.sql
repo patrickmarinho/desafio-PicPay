edd26711830bc207c89210836b38e7b392eff59f
Erick <erick.marinho2003@gmail.com>
2024-08-20 00:37:09 -0300

    refactor: remove migration script

--- src/main/resources/db/migration/V2__create-table-transactions.sql
+++
@@ -1,8 +0,0 @@
-CREATE Table transactions(
-    id SERIAL PRIMARY KEY UNIQUE NOT NULL,
-    amount DECIMAL NOT NULL,
-    payer_id BIGINT,
-    payee_id BIGINT,
-    FOREIGN KEY (payer_id) REFERENCES users(id),
-    FOREIGN KEY (payee_id) REFERENCES users(id)
-);
