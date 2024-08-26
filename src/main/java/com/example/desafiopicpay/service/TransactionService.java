841baba2cb7c99cfd828383b9d07b95b913b8cd8
Erick <erick.marinho2003@gmail.com>
2024-08-26 01:00:26 -0300

    feat: add new transaction validation

--- src/main/java/com/example/desafiopicpay/service/TransactionService.java
+++ src/main/java/com/example/desafiopicpay/service/TransactionService.java
@@ -3,63 +3,85 @@ package com.example.desafiopicpay.service;
 import com.example.desafiopicpay.domain.entity.transaction.Transaction;
 import com.example.desafiopicpay.domain.entity.user.User;
 import com.example.desafiopicpay.domain.entity.user.UserType;
+import com.example.desafiopicpay.dto.EmailDTO;
 import com.example.desafiopicpay.dto.TransactionDTO;
 import com.example.desafiopicpay.repository.TransactionRepository;
 import com.example.desafiopicpay.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.util.Map;
+
 
 @Service
 public class TransactionService {
     @Autowired
-    TransactionRepository transactionRepository;
+    private TransactionRepository transactionRepository;
+
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
+    @Autowired
+    AuthorizationService authorizationService;
 
+    @Autowired
+    NotificationService notificationService;
 
     //Post
     public TransactionDTO executeTransaction(TransactionDTO transactionDTO){
-        validateTransaction(transactionDTO);
+
+        boolean isValid = validateTransaction(transactionDTO);
+        if(!isValid){
+            throw new RuntimeException(); // change later
+        }
+
         User payer = userRepository.findUserById(transactionDTO.payerID());
         User payee = userRepository.findUserById(transactionDTO.payeeID());
-
-        boolean isAuthorized = authorizeTransaction();
-        if(!isAuthorized){
-            throw new RuntimeException("erro NÃO AUTORIZADO");
-        }
 
         updateBalances(payer, payee, transactionDTO.amount());
 
         Transaction transaction = createTransaction(transactionDTO, payer, payee);
 
-
         transactionRepository.save(transaction);
+
         return transactionDTO;
     }
 
-    private boolean authorizeTransaction(){
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "https://util.devi.tools/api/v2/authorize";
+    private boolean validateTransaction(TransactionDTO transactionDTO){
+        User payer = userRepository.findUserById(transactionDTO.payerID());
 
-        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity(url, Map.class);
+        if(payer.getCurrentBalance().compareTo(BigDecimal.ZERO) <= 0){
+            return false;
+        }
 
-        if(authorizationResponse.getStatusCode() == HttpStatus.OK){
-            return true;
-        }else return false;
+        if(payer.getCurrentBalance().compareTo(transactionDTO.amount()) < 0){
+            return false;
+        }
+
+        if(payer.getUserType() == UserType.SHOPKEEPER){
+            return false;
+        }
+
+        boolean isAuthorized = authorizationService.authorizeTransaction();
+        if(!isAuthorized){
+            return false;
+        }
+        return true;
     }
 
     private void updateBalances(User payer, User payee, BigDecimal amount) {
         payer.setCurrentBalance(payer.getCurrentBalance().subtract(amount));
         payee.setCurrentBalance(payee.getCurrentBalance().add(amount));
+
+        EmailDTO payeeNotification = new EmailDTO(payee.getEmail(),
+                "Transfer Received Successfully",
+                "You have received a transfer.");
+
+        boolean payeeReceivedNotification = notificationService.sendNotification(payeeNotification);
+        if(!payeeReceivedNotification){
+            throw new RuntimeException(); // change later
+        }
 
         userRepository.save(payer);
         userRepository.save(payee);
@@ -72,21 +94,5 @@ public class TransactionService {
         transaction.setPayee(payee);
         transaction.setTransactionDateTime(OffsetDateTime.now());
         return transaction;
-    }
-
-    private void validateTransaction(TransactionDTO transactionDTO){
-        User payer = userRepository.findUserById(transactionDTO.payerID());
-
-        if(payer.getCurrentBalance().compareTo(BigDecimal.ZERO) <= 0){
-            throw new RuntimeException(); // change later
-        }
-
-        if(payer.getCurrentBalance().compareTo(transactionDTO.amount()) < 0){
-            throw new RuntimeException(); // change later
-        }
-
-        if(payer.getUserType() == UserType.SHOPKEEPER){
-            throw new RuntimeException(); // change later
-        }
     }
 }
