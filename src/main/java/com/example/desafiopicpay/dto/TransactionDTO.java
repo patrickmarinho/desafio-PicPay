d5513f27196054af01200f5da9d84198de41f34b
Erick <erick.marinho2003@gmail.com>
2024-08-26 00:58:24 -0300

    chore: remove unnecessary imports

--- src/main/java/com/example/desafiopicpay/dto/TransactionDTO.java
+++ src/main/java/com/example/desafiopicpay/dto/TransactionDTO.java
@@ -1,7 +1,6 @@
 package com.example.desafiopicpay.dto;
 
 import java.math.BigDecimal;
-import java.time.OffsetDateTime;
 
 public record TransactionDTO(BigDecimal amount, Long payerID, Long payeeID) {
 }
